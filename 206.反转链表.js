5e2da4f98720e1dc1778b96a5a73b3a7295857af
xiangxiang <1506674357@qq.com>
2024-01-03 19:35:04 +0800

    Create 206.反转链表.js

---
+++ 206.反转链表.js
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=javascript
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+function ListNode(val, next) {
+  this.val = (val === undefined ? 0 : val)
+  this.next = (next === undefined ? null : next)
+}
+const n1 = new ListNode(1)
+const n2 = new ListNode(2)
+const n3 = new ListNode(3)
+const n4 = new ListNode(4)
+const n5 = new ListNode(5)
+n1.next = n2
+n2.next = n3
+n3.next = n4
+n4.next = n5
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  // 制造一个个的新节点，然后值和原来的对应，这样可以实现
+  // 先成双向，再删掉一向
+  // 只搞一个新节点，不断保存上一个
+  let node = new ListNode(0)
+  let current = head
+  if (current) {
+    while (current.next) {
+      let temp = current.next
+      current.next = node.next
+      node.next = current
+      current = temp
+    }
+    current.next = node.next
+  }
+  return current
+};
+// @lc code=end
