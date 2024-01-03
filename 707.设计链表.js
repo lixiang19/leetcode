0f2104eb12a760c555f782fd8ff65be1f74f3443
xiangxiang <1506674357@qq.com>
2024-01-03 19:35:08 +0800

    Update 707.设计链表.js

--- 707.设计链表.js
+++ 707.设计链表.js
@@ -3,35 +3,83 @@
  *
  * [707] 设计链表
  */
-
+class ListNode {
+  constructor(val, next) {
+    this.val = val
+    this.next = next
+  }
+}
 // @lc code=start
 
-var MyLinkedList = function() {
+var MyLinkedList = function () {
+  this.head = null
+}
+MyLinkedList.prototype.traversal = function () {
+  current = this.head
+  const list = []
+  if (current) {
 
-};
+    list.push(current.val)
+    while (current.next) {
+      current = current.next
+      list.push(current.val)
+    }
+  }
 
+
+  console.log(list)
+}
 /** 
  * @param {number} index
  * @return {number}
  */
-MyLinkedList.prototype.get = function(index) {
-
+MyLinkedList.prototype.get = function (index) {
+  current = this.head
+  let currentIndex = 0
+  if (index === 0) {
+    return this.head ? this.head.val : -1
+  }
+  let val = -1
+  while (current.next) {
+    currentIndex++
+    if (currentIndex === index) {
+      val = current.next.val
+      break
+    }
+    current = current.next
+  }
+  console.log('get的时候', val)
+  return val
 };
 
 /** 
  * @param {number} val
  * @return {void}
  */
-MyLinkedList.prototype.addAtHead = function(val) {
-
+MyLinkedList.prototype.addAtHead = function (val) {
+  const node = new ListNode(val)
+  if (this.head) {
+    node.next = this.head
+  }
+  this.head = node
 };
 
 /** 
  * @param {number} val
  * @return {void}
  */
-MyLinkedList.prototype.addAtTail = function(val) {
-
+MyLinkedList.prototype.addAtTail = function (val) {
+  const node = new ListNode(val)
+  let current
+  if (this.head) {
+    current = this.head
+    while (current.next) {
+      current = current.next
+    }
+    current.next = node
+  } else {
+    this.head = node
+  }
 };
 
 /** 
@@ -39,16 +87,54 @@ MyLinkedList.prototype.addAtTail = function(val) {
  * @param {number} val
  * @return {void}
  */
-MyLinkedList.prototype.addAtIndex = function(index, val) {
+MyLinkedList.prototype.addAtIndex = function (index, val) {
+  //将一个值为 val 的节点插入到链表中下标为 index 的节点之前。如果 index 等于链表的长度，那么该节点会被追加到链表的末尾。如果 index 比长度更大，该节点将 不会插入 到链表中。
 
+
+  let currentIndex = 0
+  const node = new ListNode(val)
+  current = this.head
+  if (index === 0) {
+    node.next = this.head
+    this.head = node
+    return
+  }
+  while (current && current.next) {
+    currentIndex++
+    if (currentIndex === index) {
+      node.next = current.next
+      current.next = node
+    } else {
+      current = current.next
+    }
+  }
+  if (current && currentIndex + 1 === index) {
+    current.next = node
+  }
 };
 
 /** 
  * @param {number} index
  * @return {void}
  */
-MyLinkedList.prototype.deleteAtIndex = function(index) {
+MyLinkedList.prototype.deleteAtIndex = function (index) {
+  let currentIndex = 0
 
+  current = this.head
+  if (index === 0) {
+    this.head = current.next
+    return
+  }
+  while (current.next) {
+    currentIndex++
+    if (currentIndex === index) {
+      // 删除的是next
+      current.next = current.next.next
+    } else {
+      current = current.next
+    }
+
+  }
 };
 
 /**
