931704c7cd7fcd7069753d2f14847b0b900db157
lixiang19 <1506674357@qq.com>
2024-08-09 16:23:47 +0800

    Create 1508.子数组和排序后的区间和.js

---
+++ 1508.子数组和排序后的区间和.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=1508 lang=javascript
+ *
+ * [1508] 子数组和排序后的区间和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+var rangeSum = function(nums, n, left, right) {
+
+};
+// @lc code=end
+
