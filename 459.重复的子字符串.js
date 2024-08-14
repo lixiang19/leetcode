369f2a3fa42df18b83b2842d811135d61252dae8
lixiang19 <1506674357@qq.com>
2024-08-14 23:06:39 +0800

    Update 459.重复的子字符串.js

--- 459.重复的子字符串.js
+++ 459.重复的子字符串.js
@@ -10,7 +10,18 @@
  * @return {boolean}
  */
 var repeatedSubstringPattern = function(s) {
-
+  let isRepeated = false;
+  for (let i = 1; i <= s.length / 2; i++) {
+    const subStr = s.slice(0, i);
+    const splitArr = s.split(subStr);
+    if (splitArr.every(item => item === '')) {
+      isRepeated = true;
+      break
+    }else {
+      continue;
+    }
+  }
+  return isRepeated;
 };
 // @lc code=end
 
